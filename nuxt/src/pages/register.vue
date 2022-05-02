ef5959d74dae744caf6ffb5408d728a7f91d7993
Musashi Okuoka <okuoka.engineer@gmail.com>
2022-05-02 14:58:30 +0900

    一旦コミット

---
+++ nuxt/src/pages/register.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-5">
+    <h1>新規登録</h1>
+    <form style="max-width:500px">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
